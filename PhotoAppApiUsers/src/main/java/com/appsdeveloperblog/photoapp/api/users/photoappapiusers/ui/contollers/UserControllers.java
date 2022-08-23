466ea4165f7192bb387138fba983c02ae92b5551
erdemsahlan <sahlanerdem@gmail.com>
2022-08-23 17:01:59 +0300

    second development on project

--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/photoappapiusers/ui/contollers/UserControllers.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/photoappapiusers/ui/contollers/UserControllers.java
@@ -1,16 +1,39 @@
 package com.appsdeveloperblog.photoapp.api.users.photoappapiusers.ui.contollers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.appsdeveloperblog.photoapp.api.users.photoappapiusers.shared.UserDto;
+import com.appsdeveloperblog.photoapp.api.users.photoappapiusers.ui.models.model;
+import com.appsdeveloperblog.photoapp.api.users.photoappapiusers.users.service.UsersService;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/users")
 public class UserControllers {
 
+        @Autowired
+         UsersService usersService;
+        @Autowired
+        private Environment env;
         @GetMapping("/status/check")
         public String status(){
-            return "service work";
+                return "Working on port " + env.getProperty("local.server.port");
+        }
+
+        @PostMapping
+        public String createUser(@Valid @RequestBody model userDetails){
+
+                ModelMapper modelMapper = new ModelMapper();
+                modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+                UserDto userDto =modelMapper.map(userDetails,UserDto.class);
+                usersService.createUser(userDto);
+
+                return "";
         }
 
 }
